0275ac4272e497634ce463b706dd8f80363e9a10
OBaller <naseemoyebola@gmail.com>
2022-05-15 19:05:15 +0100

    Initial Commit

---
+++ SearchQuery/SearchQuery/SearchQueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchQueryApp.swift
+//  SearchQuery
+//
+//  Created by Naseem Oyebola on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchQueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
